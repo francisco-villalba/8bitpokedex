e44783865b14cad2f410fc88bec7f54f9a82286c
Francisco Villalba <99042742+francisco-villalba@users.noreply.github.com>
2023-11-08 18:18:25 -0300

    Update Base.js

--- src/components/Screen/Base.js
+++ src/components/Screen/Base.js
@@ -3,7 +3,7 @@ import { Ground, Static } from './Screen.styles';
 const Base = ( {children} ) => {
     return(
         <Ground>
-            <Static src="https://thumbs.gfycat.com/HappygoluckyPolishedHartebeest-size_restricted.gif"/>
+            <Static src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGlycGhsYTBrdDNoN3V2eXE0OWc4ZTVqc3J0YzR4dGU5eTdyZmhlcCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/FOdT4x8rHYAqQ/giphy.gif"/>
             {children}
         </Ground>
     );
