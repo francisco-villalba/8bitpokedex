79c348975898485a4cded066b03cbe832b48f5d8
Francisco Villalba <99042742+francisco-villalba@users.noreply.github.com>
2023-11-08 18:24:29 -0300

    Update Base.js

--- src/components/Screen/Base.js
+++ src/components/Screen/Base.js
@@ -3,7 +3,7 @@ import { Ground, Static } from './Screen.styles';
 const Base = ( {children} ) => {
     return(
         <Ground>
-            <Static src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGlycGhsYTBrdDNoN3V2eXE0OWc4ZTVqc3J0YzR4dGU5eTdyZmhlcCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/FOdT4x8rHYAqQ/giphy.gif"/>
+            <Static src="https://i.pinimg.com/originals/5a/bb/0a/5abb0ab7950f396f6887562051d5b0de.gif"/>
             {children}
         </Ground>
     );
